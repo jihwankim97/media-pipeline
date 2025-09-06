c0ffb215d0f18191395b396bed95e4329e82a4b4
jihwankim97 <cool102476@naver.com>
2025-09-07 02:25:50 +0900

    feature: JWT Bearer 토큰 인증 미들웨어 구현 및 글로벌 적용

--- src/auth/types/request.types.ts
+++ src/auth/types/request.types.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express';
+import { JwtPayload } from './jwt.types';
 import { User } from 'src/user/entities/user.entity';
 
 export interface RequestWithUser extends Request {
-  user: User;
+  user: User | JwtPayload;
 }
